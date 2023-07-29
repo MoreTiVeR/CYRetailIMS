ad1fa554852d41005c14a8ac5a5d7969b5f9eb21
moretiver <eng.nattapong@gmail.com>
2023-07-29 20:53:41 +0700

    Initial Project

---
+++ src/ComponentService.Web/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+using ComponentService.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ComponentService.Web.Controllers;
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
