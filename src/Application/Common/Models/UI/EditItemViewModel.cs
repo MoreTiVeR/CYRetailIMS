d6bc5fd339749a543b3108b428c87efcdda8a3f1
moretiver <eng.nattapong@gmail.com>
2025-02-03 23:38:01 +0700

    Bug fix แก้ไขสินค้าไมได้เนื่องจากติด subitemtype not allow null

--- src/Application/Common/Models/UI/EditItemViewModel.cs
+++ src/Application/Common/Models/UI/EditItemViewModel.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Newtonsoft.Json;
+
 
 namespace CYRetailIMS.Application.Common.Models.UI;
 
